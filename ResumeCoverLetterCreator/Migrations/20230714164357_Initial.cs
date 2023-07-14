e400bf8fd52161de91561d8e18cb382af0736626
VladimirShch <43771483+VladimirShch@users.noreply.github.com>
2023-07-14 19:50:09 +0300

    Migrations

---
+++ ResumeCoverLetterCreator/Migrations/20230714164357_Initial.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResumeCoverLetterCreator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DocumentTags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TagName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocumentTags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DocumentTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocumentTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TagGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagGroups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TagContent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DocumentTagId = table.Column<int>(type: "int", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagContent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TagContent_DocumentTags_DocumentTagId",
+                        column: x => x.DocumentTagId,
+                        principalTable: "DocumentTags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupOptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TagGroupId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupOptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GroupOptions_TagGroups_TagGroupId",
+                        column: x => x.TagGroupId,
+                        principalTable: "TagGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupOptionContent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GroupOptionsId = table.Column<int>(type: "int", nullable: false),
+                    TagContentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupOptionContent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GroupOptionContent_GroupOptions_GroupOptionsId",
+                        column: x => x.GroupOptionsId,
+                        principalTable: "GroupOptions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GroupOptionContent_TagContent_TagContentId",
+                        column: x => x.TagContentId,
+                        principalTable: "TagContent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupOptionContent_GroupOptionsId",
+                table: "GroupOptionContent",
+                column: "GroupOptionsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupOptionContent_TagContentId",
+                table: "GroupOptionContent",
+                column: "TagContentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupOptions_TagGroupId",
+                table: "GroupOptions",
+                column: "TagGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagContent_DocumentTagId",
+                table: "TagContent",
+                column: "DocumentTagId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DocumentTypes");
+
+            migrationBuilder.DropTable(
+                name: "GroupOptionContent");
+
+            migrationBuilder.DropTable(
+                name: "GroupOptions");
+
+            migrationBuilder.DropTable(
+                name: "TagContent");
+
+            migrationBuilder.DropTable(
+                name: "TagGroups");
+
+            migrationBuilder.DropTable(
+                name: "DocumentTags");
+        }
+    }
+}
--- ResumeCoverLetterCreator/SeedData.cs
+++ ResumeCoverLetterCreator/SeedData.cs
@@ -7,7 +7,12 @@ namespace ResumeCoverLetterCreator
     {
         public static void EnsureCreated(IServiceProvider serviceProvider)
         {
-           
+            using var scopedServices = serviceProvider.CreateScope();
+            using ResumeCreatorDbContext dbContext = scopedServices.ServiceProvider.GetRequiredService<ResumeCreatorDbContext>();
+            if (dbContext.Database.GetPendingMigrations().Any())
+            {
+                dbContext.Database.Migrate();
+            }
         }
     }
 }
